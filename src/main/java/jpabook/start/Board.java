1e8c1a59531f9b84dbdc9696d102100528074591
tseop <77163842+tseop@users.noreply.github.com>
2023-01-23 23:12:07 +0900

    chapter04 - close

---
+++ src/main/java/jpabook/start/Board.java
@@ -0,0 +1,21 @@
+package jpabook.start;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(
+        name = "BOARD_SEQ_GENERATOR",
+        sequenceName = "BOARD_SEQ",
+        initialValue = 1,
+        allocationSize = 1
+)
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+}
