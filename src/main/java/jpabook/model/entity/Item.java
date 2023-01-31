5578b2b3f62cf6b9de30d5deadca088eaf2eb0bd
tseop <77163842+tseop@users.noreply.github.com>
2023-01-31 17:16:08 +0900

    feat: 카테고리 다대다 관계 매핑

--- src/main/java/jpabook/model/entity/Item.java
+++ src/main/java/jpabook/model/entity/Item.java
@@ -1,9 +1,13 @@
 package jpabook.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +19,9 @@ public class Item {
     @GeneratedValue
     @Column(name = "ITEM_ID")
     private Long id;
+
+    @OneToMany(mappedBy = "item")
+    private List<CategoryItem> categoryItems = new ArrayList<>();
 
     private String name;
     private int price;
