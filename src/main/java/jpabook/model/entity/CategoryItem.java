6f19e267219ac5480f6317c7e0435b0bfe574195
tseop <77163842+tseop@users.noreply.github.com>
2023-01-31 17:15:58 +0900

    feat: 카테고리-아이템 다대다 관계 연결 엔티티 생성

---
+++ src/main/java/jpabook/model/entity/CategoryItem.java
@@ -0,0 +1,30 @@
+package jpabook.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class CategoryItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "CATEGORY_ITEM_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "CATEGORY_ID")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+}
