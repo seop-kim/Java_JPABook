6af3461fadc82389c8fbc9b9265908718ba43141
tseop <77163842+seop-kim@users.noreply.github.com>
2023-02-08 16:29:21 +0900

    chapter07 - close

--- src/main/java/jpabook/chapter07/BoardDetail.java
+++ src/main/java/jpabook/chapter07/BoardDetail.java
@@ -1,33 +1,38 @@
 package jpabook.chapter07;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@ToString(exclude = "board")
+//@ToString(exclude = "board")
 @Entity
 public class BoardDetail {
     @Id
+    @GeneratedValue
+    @Column(name = "BOARD_DETAIL_ID")
     private Long boardId;
 
-    @MapsId
-    @OneToOne
-    @JoinColumn(name = "BOARD_ID")
-    private Board board;
+//    @MapsId
+//    @OneToOne
+//    @JoinColumn(name = "BOARD_ID")
+//    private Board board;
 
     private String content;
 
     // 연관관계 매핑
-    public void setBoard(Board board) {
-        this.board = board;
-        board.setBoardDetail(this);
-    }
+//    public void setBoard(Board board) {
+//        this.board = board;
+//        board.setBoardDetail(this);
+//    }
 }
